24dad0e12c784f9b973145d029729e7c98ddef31
durachok <acella06@ya.ru>
2024-10-25 11:40:50 +0300

    Сложение

---
+++ Котеян и Филатов/varyakras/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varyakras
+{
+    internal class Animal
+    {
+        public string color = "white";
+        public string voice = "meow-meow";
+        public bool tail = true;
+        public string name = "Princess";
+        public string sex = "woman";
+
+        public string GetVoice() {
+            return voice;
+        }
+    }
+
+}
---
+++ Котеян и Филатов/varyakras/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace varyakras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            string voice = animal.GetVoice();
+            Console.WriteLine(voice);
+        }
+    }
+}
