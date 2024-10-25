98059aea272eaa3d3d0313165a0144fc3b3b78e3
durachok <acella06@ya.ru>
2024-10-25 11:44:27 +0300

    Вычитание

---
+++ Котеян и Филатов/канкулятор сумма и вычиьтание/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace канкулятор_сумма_и_вычиьтание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                // Ввод первого числа
+                Console.Write("Введите первое число: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                // Ввод второго числа
+                Console.Write("Введите второе число: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                // Сложение
+                double sum = num1 + num2;
+                Console.WriteLine($"Сумма: {num1} + {num2} = {sum}");
+
+            double min = num1 - num2;
+            Console.WriteLine($"Сумма: {num1} - {num2} = {min}");
+
+            Console.ReadLine();
+           ;
+        }
+    }
+}
