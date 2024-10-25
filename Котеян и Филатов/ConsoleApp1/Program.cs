fcbb25bb8b5be41a841472cb40e9cfa7a0bfc9b1
durachok <acella06@ya.ru>
2024-10-25 11:52:10 +0300

    Координаты точек

--- Котеян и Филатов/ConsoleApp1/Program.cs
+++ Котеян и Филатов/ConsoleApp1/Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            // CIRCLE
+            //Координаты точек
         }
     }
 }
